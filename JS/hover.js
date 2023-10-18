e0246720b4413e7dec87a649f1987a6854e95cdf
mangtrungzos <sangv906@gmail.com>
2023-10-18 15:25:03 +0700

    18/10

---
+++ JS/hover.js
@@ -0,0 +1,10 @@
+// Hover show image
+function showHoverImage() {
+    const hoverImage = document.querySelector('.hover-image');
+    hoverImage.classList.add('active')
+}
+
+// function hideHoverImage() {
+//     const hoverImage = document.querySelector('.hover-image');
+//     hoverImage.classList.add('hide')
+// }
