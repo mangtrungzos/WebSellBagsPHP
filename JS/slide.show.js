c9cf7d04041a5b7aeece62733c582159d8a2940b
mangtrungzos <sangv906@gmail.com>
2023-10-25 00:53:40 +0700

    25/10

---
+++ JS/slide.show.js
@@ -0,0 +1,45 @@
+const listImage = document.querySelector('.list-images');
+const imgs = document.querySelectorAll('.img-detail')
+const length = imgs.length
+const btnLeft = document.querySelector('.btn-left')
+const btnRight = document.querySelector('.btn-right')
+let current = 0
+
+// const handleChangeSlide = () => {
+//   if (current == length - 1) {
+//     current = 0
+//     let width = imgs[0].offsetWidth
+//     listImage.style.transform = `translateX(0)`
+//   } else{
+//     let width = imgs[0].offsetWidth
+//     listImage.style.transform = `translateX(${width * -1 * current}px)`
+//   }
+  
+// }
+
+const handleChangeSlide = () => {
+  const width = imgs[0].offsetWidth;
+  const maxTranslateX = (length - 1) * width * -1; // Giới hạn tối đa của translateX
+
+  if (current == length - 1) {
+    current = 0;
+    listImage.style.transform = `translateX(0)`;
+  } else {
+    let translateX = width * -1 * current;
+    if (translateX < maxTranslateX) {
+      translateX = maxTranslateX; // Đảm bảo không vượt quá giới hạn tối đa
+    }
+    listImage.style.transform = `translateX(${translateX}px)`;
+  }
+};
+
+btnRight.addEventListener('click', () => {
+  current ++
+  handleChangeSlide()
+})
+
+btnLeft.addEventListener('click', () => {
+  current --
+  handleChangeSlide()
+})
+
